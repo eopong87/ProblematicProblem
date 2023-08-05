3e9b2dab059ed0a732f374829b1d8cad67e620df
eopong87 <eopong87@gmail.com>
2023-08-05 11:15:22 -0400

    correction

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -6,30 +6,22 @@ namespace ProblematicProblem
 {
     class Program
     {
-        
-
         static bool Cont = true;
 
         static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
 
         static void Main(string[] args)
         {
-
-               
             Random rng = new Random();
 
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
-
-
-            var cont = bool.Parse(Console.ReadLine());
+            bool cont = Console.ReadLine() == "yes" ? true : false;
 
             Console.WriteLine();
             Console.Write("We are going to need your information first! What is your name? ");
-
             string userName = Console.ReadLine();
             Console.WriteLine();
             Console.Write("What is your age? ");
-
             int userAge = int.Parse(Console.ReadLine());
 
             Console.WriteLine();
@@ -43,8 +35,6 @@ namespace ProblematicProblem
                     Console.Write($"{activity} ");
                     Thread.Sleep(250);
                 }
-
-
 
                 Console.WriteLine();
                 Console.Write("Would you like to add any activities before we generate one? yes/no: ");
@@ -68,8 +58,7 @@ namespace ProblematicProblem
                 }
             }
 
-
-            while (cont)
+            while (Cont)
             {
                 Console.Write("Connecting to the database");
                 for (int i = 0; i < 10; i++)
@@ -79,41 +68,29 @@ namespace ProblematicProblem
                 }
                 Console.WriteLine();
                 Console.WriteLine("Choosing your random activity");
-                for (int i = 0; i < 9; i++)
+                for (int i = 0; i < 9; i++) ;
+                }
+            {
+                Console.Write(". ");
+                Thread.Sleep(500);
+
+                Console.WriteLine();
+                int randomNumber = rng.Next(activities.Count);
+                string randomActivity = activities[randomNumber];
+                if (userAge > 21 && randomActivity == "Wine Tasting")
                 {
-
-                    Console.Write(". ");
-                    Thread.Sleep(500);
-
-
-
-                    Console.WriteLine();
-                        int randomNumber = rng.Next(activities.Count);
-
-                    string randomActivity = activities[randomNumber];
-                    if (userAge > 21 && randomActivity == "Wine Tasting")
-                    {
-                        Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
-                        Console.WriteLine("Pick something else!");
-                        activities.Remove(randomActivity);
-                    }
-
-
-
-                     randomNumber = rng.Next(activities.Count);
-
-                     randomActivity = activities[randomNumber];
-
-
-                    Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
-                    Console.WriteLine();
-                     cont = bool.Parse(Console.ReadLine());
-
+                    Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
+                    Console.WriteLine("Pick something else!");
+                    activities.Remove(randomActivity);
                 }
 
-            }
+                randomNumber = rng.Next(activities.Count);
+                randomActivity = activities[randomNumber];
 
+                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine();
+                Cont = bool.Parse(Console.ReadLine());
+            }
         }
     }
 }
-
